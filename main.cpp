e285908017843226d59c4f9312472351e25ad7de
Galit Lebedev <galitlebedev2@gmail.com>
2024-06-24 20:54:50 +0300

    Verify function add in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <fstream>
 #include "BlockChain.h"
+#include "Utilities.h"
 
-void format(std::ifstream &in_file, std::ofstream &out_file) {
+using std::string;
+using std::ifstream;
+using std::ofstream;
+
+void format(ifstream &in_file, ofstream &out_file) {
     BlockChain block = BlockChainLoad(in_file);
     BlockChainDump(block, out_file);
 }
 
-void hash(std::ifstream &in_file, std::ofstream &out_file){
+void hash(ifstream &in_file, ofstream &out_file){
     BlockChain block = BlockChainLoad(in_file);
     BlockChainDumpHashed(block, out_file);
 }
-void compress(std::ifstream &in_file, std::ofstream &out_file) {
+void compress(ifstream &in_file, ofstream &out_file) {
     BlockChain block = BlockChainLoad(in_file);
     BlockChainDump(block, out_file);
     BlockChainCompress(block);
 }
+void verify(ifstream &source, ofstream &target) {
+    BlockChain block = BlockChainLoad(source);
 
+}
 int main(int argc, char **argv) {
     //  std::cout << "Hello, World!" << std::endl;
     if(argc != 4){
+        getErrorMessage();
         return 0;
     }
-    const std::string &sourcefile = argv[2];
-    const std::string &target = argv[3];
+    const string &sourcefile = argv[2];
+    const string &target = argv[3];
 
-    std::ifstream in_file(sourcefile);
-    std::ofstream out_file(target);
+    ifstream in_file(sourcefile);
+    ofstream out_file(target);
 
-    if (argv[1] == "format") {
+    if (strcmp(argv[1],"format") == 0) {
         format(in_file,out_file);
     }
-    if (argv[1] == "hash") {
+    if (strcmp(argv[1],"hash") == 0) {
         hash(in_file,out_file);
     }
-    if (argv[1] == "compress") {
+    if (strcmp(argv[1],"compress") == 0) {
         compress(in_file,out_file);
+    }
+    if (strcmp(argv[1],"verify") == 0) {
+        verify(in_file,out_file);
     }
     in_file.close();
     out_file.close();
