1880bbb4cc17aa95ef7a0a447ff2dad315c49da7
Galit Lebedev <galitlebedev2@gmail.com>
2024-06-24 21:07:27 +0300

    Verify function UPDATE

--- main.cpp
+++ main.cpp
@@ -21,35 +21,46 @@ void compress(ifstream &in_file, ofstream &out_file) {
     BlockChainDump(block, out_file);
     BlockChainCompress(block);
 }
-void verify(ifstream &source, ofstream &target) {
+void verify(ifstream &source, ifstream &target) {
     BlockChain block = BlockChainLoad(source);
-
+    if(BlockChainVerifyFile(block,target)){
+        std::cout << "Verification passed" << std::endl;
+    }
+    else std::cout << "Verification failed" << std::endl;
 }
 int main(int argc, char **argv) {
+
     //  std::cout << "Hello, World!" << std::endl;
     if(argc != 4){
         getErrorMessage();
+        std::cout << "Usage: Usage: ./mtm_blockchain <op> <source> <target>" << std::endl;
         return 0;
     }
     const string &sourcefile = argv[2];
     const string &target = argv[3];
 
     ifstream in_file(sourcefile);
-    ofstream out_file(target);
 
     if (strcmp(argv[1],"format") == 0) {
+        ofstream out_file(target);
         format(in_file,out_file);
+        out_file.close();
     }
     if (strcmp(argv[1],"hash") == 0) {
+        ofstream out_file(target);
         hash(in_file,out_file);
+        out_file.close();
     }
     if (strcmp(argv[1],"compress") == 0) {
+        ofstream out_file(target);
         compress(in_file,out_file);
+        out_file.close();
     }
     if (strcmp(argv[1],"verify") == 0) {
+        ifstream out_file(target);
         verify(in_file,out_file);
+        out_file.close();
     }
     in_file.close();
-    out_file.close();
     return 0;
 }
